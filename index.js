2ab031492a5a412c279e34e8b1c9e67b2fa7ebf7
Chris Casad <ccasad@gmail.com>
2021-12-18 08:11:05 -0500

    changes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const _ = require("lodash");
 const fs = require("fs");
 
-// * export each distance buffer layer into it's own geojson file
-// (have 1-8hrs) possibly using th TravelTime API 
+// * DONE export each distance buffer layer into it's own geojson file
+// * loop through all the colleges in colleges.geojson to find which ones are within each distance buffer
+//   
+
 // https://docs.traveltime.com/api/reference/isochrones
 // Application ID: 36c52bfc
 // API key:  b1a8a6f648bb1e3b030825f152df8177
 
+const data = await fs.readFileSync("./colleges.geojson", "utf8");
+console.log(data);
